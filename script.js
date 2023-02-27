be42cbeae458b6f843d4c1050d0272abc740609e
mohit <54810032+mohitgupta-16@users.noreply.github.com>
2023-02-27 13:58:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
 
+// select the list element
+const list = document.querySelector('#infi-list');
+
+const listItems = list.children;
+
+function addItems() {
+  for (let i = 0; i < 2; i++) {
+    const listItem = document.createElement('li');
+    listItem.innerText = `List Item ${listItems.length + i + 1}`;
+    list.appendChild(listItem);
+  }
+}
+
+for (let i = 0; i < 10; i++) {
+  const listItem = document.createElement('li');
+  listItem.innerText = `List Item ${i + 1}`;
+  list.appendChild(listItem);
+}
+
+window.addEventListener('scroll', () => {
+	if(window.scrollY + window.innerHeight >= 
+    document.documentElement.scrollHeight)
+    addItems();
+});
